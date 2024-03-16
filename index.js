9691f93d789a6dd17a05d545cb346cc1fa9a74fe
Tracy Karanja <chirutracy@gmail.com>
2024-03-16 14:41:15 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let empty = document.getElementById("main");
+empty.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Tracy is the champion";
